95daf1fdf9820dfbca4ea92b3e7ee764f53b0e11
Damien Shaw <damien.shaw@brands2life.com>
2016-02-07 21:15:26 +0000

    first commit

---
+++ js/controllers.js
@@ -0,0 +1,31 @@
+(function(){
+
+	var artistsController = angular.module('artistsController', ['ngAnimate']);
+
+	artistsController.controller('ListController',[ '$scope', '$http',  function ($scope, $http) {
+		$http.get('js/data.json').success(function (data){
+			$scope.artists = data;
+			$scope.artistsOrder = 'name';
+		});
+		
+	}]);
+
+	artistsController.controller('DetailsController', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
+		$http.get('js/data.json').success(function (data){
+			$scope.artists = data;
+			$scope.whichArtist = $routeParams.itemId;
+
+			if($routeParams.itemId > 0) {
+				$scope.prevItem = Number($routeParams.itemId)-1;
+			} else {
+				$scope.prevItem = $scope.artists.length-1;
+			}
+
+			if($routeParams.itemId < $scope.artists.length-1) {
+				$scope.nextItem = Number($routeParams.itemId)+1;
+			} else {
+				$scope.nextItem = 0;
+			}
+		});
+	}]);
+})();
